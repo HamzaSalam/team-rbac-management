114f0ca694e292920e750c4d525393bda4d33b95
Hamza Salam <hamza.salam786@gmail.com>
2026-07-13 18:19:37 +0500

    make const folder to add const value

---
+++ app/const/index.tsx
@@ -0,0 +1,14 @@
+export const featureNames = [
+  { name: "Role Based Access Control (RBAC)" },
+  { name: "Route Protection with middleware" },
+  { name: "Server Side Permissions Checks" },
+  { name: "Client Side Permissions Hooks" },
+  { name: "Dynamic Route Access" },
+];
+
+export const roleNames = [
+  { name: "Full system access", role: "Super Admin" },
+  { name: "User and Team management", role: "Admin" },
+  { name: "Team Specific Manager", role: "Manager" },
+  { name: "Basic Dashboard", role: "User" },
+];
